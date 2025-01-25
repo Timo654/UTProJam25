3da2bfa7b8005402661684b7b12e511316b1d295
Timo654 <41972888+Timo654@users.noreply.github.com>
2025-01-25 16:16:40 +0200

    change bpm test song

--- UTProJam25/Assets/Scripts/BPMTest.cs
+++ UTProJam25/Assets/Scripts/BPMTest.cs
@@ -5,7 +5,7 @@ public class BPMTest : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        AudioManager.Instance.InitializeMusic(FMODEvents.Instance.BPMTest);
+        AudioManager.Instance.InitializeMusic(FMODEvents.Instance.BPM90Test);
     }
 
     private void Start()
