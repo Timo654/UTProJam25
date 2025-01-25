44ac443303137e1c057fd5f9a36c997312b572ed
Timo654 <41972888+Timo654@users.noreply.github.com>
2025-01-25 18:10:32 +0200

    move stuff, add high score stuff, add level script

--- UTProJam25/Assets/Scripts/BPMTest.cs
+++ UTProJam25/Assets/Scripts/BPMTest.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class BPMTest : MonoBehaviour
 {
+    [SerializeField] private LevelData levelData;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        AudioManager.Instance.InitializeMusic(FMODEvents.Instance.BPM90Test);
+        var _beatManager = FindAnyObjectByType<BeatManager>();
+        if (_beatManager != null)
+            _beatManager.SetBPM(levelData.levelBPM);
+        else Debug.LogWarning("how is the beat manager gone ????????");
+        AudioManager.Instance.InitializeMusic(levelData.levelBGM);
     }
 
     private void Start()
