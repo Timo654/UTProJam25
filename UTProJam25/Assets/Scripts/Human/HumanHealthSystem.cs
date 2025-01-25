7f8a95eea4163d6be4843cf263c0fb3298de1921
marek pihel <marek.pihel@ut.ee>
2025-01-25 21:39:51 +0200

    Humans move to water and submerge.

--- UTProJam25/Assets/Scripts/Human/HumanHealthSystem.cs
+++ UTProJam25/Assets/Scripts/Human/HumanHealthSystem.cs
@@ -5,14 +5,17 @@ using UnityEngine.UI;
 
 public class HumanHealthSystem : MonoBehaviour
 {
-    [SerializeField]
-    private HealthBarLogic healthBarLogic;
+    [SerializeField] private HealthBarLogic healthBarLogic;
     [SerializeField] private HumanTimer humanTimer;
+    [SerializeField] private HumanController humanController;
+    
     private int maxHealth = 100;
     private int currentHealth = 100;
     private float cooldownTimer = 5f;
     private float timerIncreaseOnHit = 0.5f;
     public static event Action<int> AddScoreOnDeath;
+    
+    
 
     public void InitializeHumanStats(int maxHealth, float cooldownTimer, float timerIncreaseOnHit)
     {
@@ -46,6 +49,7 @@ public class HumanHealthSystem : MonoBehaviour
         currentHealth -= 1;
         cooldownTimer += timerIncreaseOnHit;
         healthBarLogic.UpdateHealth(currentHealth, maxHealth);
+        humanController.MoveTowardsWater(maxHealth);
         if (currentHealth <= 0) HandleDeath();
         //Add highscore
     }
