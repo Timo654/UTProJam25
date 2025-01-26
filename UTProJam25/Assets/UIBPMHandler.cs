9818510b63581d15375eba0e499f716df921f38e
Timo654 <41972888+Timo654@users.noreply.github.com>
2025-01-26 02:20:11 +0200

    use actual BPM for visual indicator

---
+++ UTProJam25/Assets/UIBPMHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class UIBPMHandler : MonoBehaviour
+{
+    ParticleSystem _particleSystem;
+
+    private void Awake()
+    {
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
+        if (SaveManager.Instance.runtimeData.currentLevel != null)
+        {
+            SetupBPM(SaveManager.Instance.runtimeData.currentLevel);
+        }
+    }
+    private void OnEnable()
+    {
+        LevelLoader.OnGameplayLevelLoaded += SetupBPM;
+    }
+
+    private void OnDisable()
+    {
+        LevelLoader.OnGameplayLevelLoaded -= SetupBPM;
+    }
+
+    private void SetupBPM(LevelData data)
+    {
+        var main = _particleSystem.main;
+        main.simulationSpeed = data.levelBPM / 60f;
+        //Debug.Log($"UI BPM simulation speed is {main.simulationSpeed}");
+    }
+}
