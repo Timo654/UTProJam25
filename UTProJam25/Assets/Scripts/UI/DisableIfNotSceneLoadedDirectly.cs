7a370889d0e95093c5a86fc5db0d1f8e36f82c15
Timo654 <41972888+Timo654@users.noreply.github.com>
2025-01-26 08:26:51 +0200

    fix the black ui disabler

--- UTProJam25/Assets/Scripts/UI/DisableIfNotSceneLoadedDirectly.cs
+++ UTProJam25/Assets/Scripts/UI/DisableIfNotSceneLoadedDirectly.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DisableIfNotSceneLoadedDirectly : MonoBehaviour
 {
     // this is to stop that annoying black screen from hopefully ever appearing again during testing
     void Awake()
     {
-        if (SaveManager.Instance.runtimeData.previousSceneName.Length > 0 )
+        if (SaveManager.Instance.runtimeData.previousSceneName != null || SceneManager.GetActiveScene().name != "Ui")
             gameObject.SetActive(false);
     }
 }
